209a5e2ce500e0d3890e770928c155e01614f5e8
lucasgon <lukasz.gonczarik@gmail.com>
2017-06-11 15:19:08 +0200

    Updatd pom to working version

--- DietApp/src/main/java/dietapp/DietApp.java
+++ DietApp/src/main/java/dietapp/DietApp.java
@@ -19,8 +19,7 @@ public class DietApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("dietapp/FXMLDocument.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
