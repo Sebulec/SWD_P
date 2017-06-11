721883d3d93635af96d5271767e452f2bb8cc0fd
frasz <nataliaadamus94@gmail.com>
2017-06-11 21:29:26 +0200

    FXML file

--- DietApp/src/main/java/dietapp/FXMLDocumentController.java
+++ DietApp/src/main/java/dietapp/FXMLDocumentController.java
@@ -15,6 +15,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -24,6 +25,10 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private Label label;
+
+    @FXML
+    private TextField height;
+
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
