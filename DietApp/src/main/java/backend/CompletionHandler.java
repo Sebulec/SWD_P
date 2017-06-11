377334d26f145ecd5011f3ca1ba6cbbee5df24ae
Sebulec <sebastian.kotarski94@gmail.com>
2017-06-11 21:24:54 +0200

    CompletionHandler and Recipe implemented.

---
+++ DietApp/src/main/java/backend/CompletionHandler.java
@@ -0,0 +1,9 @@
+package backend;
+
+/**
+ * Created by sebastiankotarski on 11.06.2017.
+ */
+public interface CompletionHandler {
+    public void completed();
+    public void stopped();
+}
---
+++ DietApp/src/main/java/backend/DecisionMaker.java
@@ -0,0 +1,19 @@
+package backend;
+
+/**
+ * Created by sebastiankotarski on 11.06.2017.
+ */
+public class DecisionMaker {
+
+    public Recipe[] makeDecision(User user, RecipeType recipeType, CompletionHandler completionHandler) {
+        Recipe[] recipes = null;
+
+        return null;
+    }
+
+    public Recipe[] makeDecision(User user, CaloriesLevel caloriesLevel, CompletionHandler completionHandler) {
+        Recipe[] recipes = null;
+
+        return null;
+    }
+}
