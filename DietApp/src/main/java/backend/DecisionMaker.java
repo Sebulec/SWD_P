8b1e85872c9c4a9c6d16c01a68ea418fc4b5dbde
Sebulec <sebastian.kotarski94@gmail.com>
2017-06-12 17:34:22 +0200

    DecisionMaker implementation started.

--- DietApp/src/main/java/backend/DecisionMaker.java
+++ DietApp/src/main/java/backend/DecisionMaker.java
@@ -1,5 +1,10 @@
 package backend;
 
+import javax.security.auth.login.LoginContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by sebastiankotarski on 11.06.2017.
  */
@@ -7,13 +12,35 @@ public class DecisionMaker {
 
     public Recipe[] makeDecision(User user, RecipeType recipeType, CompletionHandler completionHandler) {
         Recipe[] recipes = null;
-
-        return null;
+        LogicFunctions logicFunctions = new LogicFunctions();
+        switch (recipeType) {
+            case breakfast:
+                logicFunctions.alphaU(4);
+                break;
+            case dinner:
+                logicFunctions.alphaU(5);
+                break;
+            case supper:
+                logicFunctions.alphaU(6);
+                break;
+        }
+        switch (user.activityType) {
+            case low:
+                logicFunctions.alphaU(1);
+                break;
+            case normal:
+                logicFunctions.alphaU(2);
+                break;
+            case high:
+                logicFunctions.alphaU(3);
+                break;
+        }
+        return recipes;
     }
 
-    public Recipe[] makeDecision(User user, CaloriesLevel caloriesLevel, CompletionHandler completionHandler) {
-        Recipe[] recipes = null;
-
-        return null;
-    }
+//    public Recipe[] makeDecision(User user, CaloriesLevel caloriesLevel, CompletionHandler completionHandler) {
+//        Recipe[] recipes = null;
+//
+//        return null;
+//    }
 }
